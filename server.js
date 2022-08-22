625873af7a7756ff9c64b64cfd58db393aa211a6
tech-shamp <shampytechnical@gmail.com>
2022-08-22 17:02:14 +0500

    favicon

--- server.js
+++ server.js
@@ -5,27 +5,46 @@ const app = express()
 const port = process.env.PORT || 999
 const path = Path.join(__dirname, "public")
 
+const titles = {
+  tag: " - Css Overview",
+  home: "Home",
+  about: "About",
+  blog: "Blog",
+  contact: "Contact",
+}
+
 app.use(express.static(path))
 app.set("view engine", "ejs")
+app.use("/css", express.static(Path.join(path, "css")))
+app.use("/js", express.static(Path.join(path, "js")))
+app.use("/img", express.static(Path.join(path, "img")))
 
 app.get("/", (req, res) => {
   console.log(req.url)
-  res.render("index")
+  res.render("index", {
+    title: titles.home + titles.tag,
+  })
 })
 
 app.get("/about", (req, res) => {
   console.log(req.url)
-  res.render("about")
+  res.render("about", {
+    title: titles.about + titles.tag,
+  })
 })
 
 app.get("/blog", (req, res) => {
   console.log(req.url)
-  res.render("blog")
+  res.render("blog", {
+    title: titles.blog + titles.tag,
+  })
 })
 
 app.get("/contact", (req, res) => {
   console.log(req.url)
-  res.render("contact")
+  res.render("contact", {
+    title: titles.contact + titles.tag,
+  })
 })
 
 app.get("*", (req, res) => {
